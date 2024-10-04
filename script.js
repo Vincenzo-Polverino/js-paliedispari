2c4197d9953a34d75b04cf2a2e4de1dc4c2fbae7
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-04 16:03:13 +0200

    test_if_script_work

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('linked');
+console.log('linked')
+
+
+/* Strumenti
+- Function
+- let
+- split
+- reverse
+- join
+- if / else
+- prompt
+- alert
+- return */
+
+let word =('anna');
+let reverse_word;
+
+reverse_word = word.split('').reverse().join('')
+
+    if (word === reverse_word) {
+
+        console.log('Parola palindroma');
+ 
+
+
+    } else {
+        console.log('Parola non palindroma');
+        
+    }
+
