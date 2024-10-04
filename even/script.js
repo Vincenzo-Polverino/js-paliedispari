9a224f16ac144d1f09b95b2aee23e25b970a9e7d
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-04 16:39:47 +0200

    add: even / uneven choice prompt

---
+++ even/script.js
@@ -0,0 +1,19 @@
+console.log('linked')
+
+let choice = prompt("Scegli Pari o Dispari"); {
+
+    if (choice === ('pari') || choice === ('Pari')) {
+
+        console.log('pippo');
+
+
+
+    } else if (choice === ('Dispari') || choice === ('dispari')) {
+        console.log('gino');
+
+    }else{
+        console.log('Scelta non valida');
+        
+    }
+}
+
