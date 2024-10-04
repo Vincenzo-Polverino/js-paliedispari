46447f394e2c854263b6f4ce195189389524fc6d
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-04 17:27:12 +0200

    fix: uppercase error

--- even/script.js
+++ even/script.js
@@ -5,40 +5,41 @@ console.log('linked')
 
 
 function dice_game(choice, user_roll) {
+    choice = choice.toLowerCase();
 
-let cpu_roll = Math.floor((Math.random() * 5) + 1);
+    let cpu_roll = Math.floor((Math.random() * 5) + 1);
 
-if ((user_roll + cpu_roll) % 2 === 0 && choice === ('pari')) {
-    console.log('hai vinto!');
-    console.log(user_roll);
-    console.log(cpu_roll);
-    console.log(choice);
-    
-
-} else if
-    ((user_roll + cpu_roll) % 2 !== 0 && choice === ('dispari')) {
-    console.log('hai vinto!');
-    console.log(user_roll);
-    console.log(cpu_roll);
-    console.log(choice);
-
-} else if ((user_roll + cpu_roll) % 2 === 0 && choice === ('dispari')){
-    console.log('hai perso');
-    console.log(user_roll);
-    console.log(cpu_roll);
-    console.log(choice);
-    
-}else if ((user_roll + cpu_roll) % 2 !== 0 && choice === ('pari')){
-    console.log('hai perso');
-    console.log(user_roll);
-    console.log(cpu_roll);
-    console.log(choice);
+    if ((user_roll + cpu_roll) % 2 === 0 && choice === ('pari')) {
+        console.log('hai vinto!');
+        console.log(user_roll);
+        console.log(cpu_roll);
+        console.log(choice);
 
 
-}else{
-    console.log('Valore non valido');
-    
+    } else if
+        ((user_roll + cpu_roll) % 2 !== 0 && choice === ('dispari')) {
+        console.log('hai vinto!');
+        console.log(user_roll);
+        console.log(cpu_roll);
+        console.log(choice);
+
+    } else if ((user_roll + cpu_roll) % 2 === 0 && choice === ('dispari')) {
+        console.log('hai perso');
+        console.log(user_roll);
+        console.log(cpu_roll);
+        console.log(choice);
+
+    } else if ((user_roll + cpu_roll) % 2 !== 0 && choice === ('pari')) {
+        console.log('hai perso');
+        console.log(user_roll);
+        console.log(cpu_roll);
+        console.log(choice);
+
+
+    } else {
+        console.log('Valore non valido');
+
+    }
 }
-}
 
-dice_game(prompt("Scegli Pari o Dispari"),Number(prompt('Scegli un numero da 1 a 5')))
+dice_game(prompt("Scegli Pari o Dispari"), Number(prompt('Scegli un numero da 1 a 5')))
