50623f207deeac0ecd1822f68936bf3310d7c5cf
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-04 17:14:11 +0200

    fix choise script and add roll script

--- even/script.js
+++ even/script.js
@@ -1,19 +1,40 @@
 console.log('linked')
 
-let choice = prompt("Scegli Pari o Dispari"); {
-
-    if (choice === ('pari') || choice === ('Pari')) {
-
-        console.log('pippo');
+let choice = prompt("Scegli Pari o Dispari")
 
 
+let user_roll = Number(prompt('Scegli un numero da 1 a 5'))
 
-    } else if (choice === ('Dispari') || choice === ('dispari')) {
-        console.log('gino');
+let cpu_roll = Math.floor((Math.random() * 5) + 1);
 
-    }else{
-        console.log('Scelta non valida');
-        
-    }
+if ((user_roll + cpu_roll) % 2 === 0 && choice === ('pari')) {
+    console.log('hai vinto!');
+    console.log(user_roll);
+    console.log(cpu_roll);
+    console.log(choice);
+    
+
+} else if
+    ((user_roll + cpu_roll) % 2 !== 0 && choice === ('dispari')) {
+    console.log('hai vinto!');
+    console.log(user_roll);
+    console.log(cpu_roll);
+    console.log(choice);
+
+} else if ((user_roll + cpu_roll) % 2 === 0 && choice === ('dispari')){
+    console.log('hai perso');
+    console.log(user_roll);
+    console.log(cpu_roll);
+    console.log(choice);
+    
+}else if ((user_roll + cpu_roll) % 2 !== 0 && choice === ('pari')){
+    console.log('hai perso');
+    console.log(user_roll);
+    console.log(cpu_roll);
+    console.log(choice);
+
+
+}else{
+    console.log('Valore non valido');
+    
 }
-
